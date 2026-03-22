c2ea886bebc6a9c00c78a76e8259c6bc9670406e
forforcio <enrique.ortega.cabello@proton.me>
2026-03-22 19:48:10 +0100

    #5 : delete unused classes

--- app/src/main/java/com/example/faketagram/data/model/Chat.kt
+++
@@ -1,9 +0,0 @@
-package com.example.faketagram.data.model
-
-import kotlinx.serialization.Serializable
-
-
-data class Chat(
-    var userId: Int,
-    var messages: List<Message> = emptyList()) {
-}
