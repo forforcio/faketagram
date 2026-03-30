68988495ed6f139c6bb90cf266bfd0c31e59f8a1
forforcio <enrique.ortega.cabello@proton.me>
2026-03-30 16:56:30 +0200

    #80 : change chat cursor an underline color

--- app/src/main/java/com/izzo/meelt/ui/nav/StartUserChatScreen.kt
+++ app/src/main/java/com/izzo/meelt/ui/nav/StartUserChatScreen.kt
@@ -201,6 +201,10 @@ fun StartUserChatScreen(
                             unfocusedContainerColor = MaterialTheme.colorScheme.surface,
                             disabledContainerColor = MaterialTheme.colorScheme.surface,
                             errorContainerColor = MaterialTheme.colorScheme.surface,
+                            focusedIndicatorColor = Color.Transparent,
+                            unfocusedIndicatorColor = Color.Transparent,
+                            disabledIndicatorColor = Color.Transparent,
+                            cursorColor = Color.Black,
                         ),
                     )
                     Icon(
