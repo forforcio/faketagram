888f5461757813e1d0832192da5cb9bcd097adae
forforcio <enrique.ortega.cabello@proton.me>
2026-04-01 12:10:52 +0200

    #84 : increase feed titles size

--- app/src/main/java/com/izzo/meelt/ui/nav/StartFeedScreen.kt
+++ app/src/main/java/com/izzo/meelt/ui/nav/StartFeedScreen.kt
@@ -26,7 +26,9 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.izzo.meelt.R
 import com.izzo.meelt.data.UsersUiState
 import com.izzo.meelt.data.model.User
